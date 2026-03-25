a1fae71abdd4d152837bb061671245f3c6c8b7e4
nitta <t-nitta@cuolega.co.jp>
2026-03-25 13:36:42 +0900

    [add]デザイン微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,33 @@ import { EntryForm } from '../components/entryForm'
 export default function Home() {
   return (
     <div className="min-h-screen border-0 outline-none">
-      <FirstView
-        weddingDate="2026年7月18日（土）"
-        weddingDateTime="2026-07-18"
-      />
-      <CountDown />
-      <Message />
-      <Profile />
-      <Album />
-      <PartyInfo />
-      <Other />
-      <EntryForm responseDeadline="2026年○月○日" />
+      <div className="pageSnapSection">
+        <FirstView
+          weddingDate="2026年7月18日（土）"
+          weddingDateTime="2026-07-18"
+        />
+      </div>
+      <div className="pageSnapSection">
+        <CountDown />
+      </div>
+      <div className="pageSnapSection">
+        <Message />
+      </div>
+      <div className="pageSnapSection">
+        <Profile />
+      </div>
+      <div className="pageSnapSection">
+        <Album />
+      </div>
+      <div className="pageSnapSection">
+        <PartyInfo />
+      </div>
+      <div className="pageSnapSection">
+        <Other />
+      </div>
+      <div className="pageSnapSectionLong">
+        <EntryForm responseDeadline="2026年○月○日" />
+      </div>
     </div>
   )
 }
